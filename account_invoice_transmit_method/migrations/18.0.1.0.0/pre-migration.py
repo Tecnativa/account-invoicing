b7aa534c6d45f08f04916ee2e53b909eb85a6aaa
sergio-teruel <sergio.teruel@tecnativa.com>
2026-04-19 04:04:14 +0200

    [IMP][NO-MERGE] account_invoice_transmit_method: pre and post mig

---
+++ account_invoice_transmit_method/migrations/18.0.1.0.0/pre-migration.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+_column_renames = {
+    "res_partner": [
+        ("customer_invoice_transmit_method_id", None),
+        ("supplier_invoice_transmit_method_id", None),
+    ]
+}
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_columns(env.cr, _column_renames)
